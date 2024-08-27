59eed8480a35edb4cba2fe310cfc428a25c0d9cf
DoryanGrayBolado38 <lukinhasdeoliveiramoraes@gmail.com>
2024-08-26 22:46:43 -0300

    Firebase

---
+++ Rotas.js
@@ -0,0 +1,14 @@
+import { React } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { home } from "home";
+import { Cadastrar } from "./CadastrarCriptos";
+import { Alterar } from "./AlterarCriptos";
+
+const stack = createStackNavigator();
+
+export default function rotas() {
+    return(
+        
+    );
+}
